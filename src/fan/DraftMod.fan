afrankvt/draft

commit c8e99282bd2216398b0b95f604e365d34e8a58f5

Fixup `DraftMod.onServicePod` to catch and ignore broken socket pipe errs
These seem to occur alot with FileWeblet; and add alot of noise to logging.
Seems safe to just ignore them when they occur and suppress exception.

src/fan/DraftMod.fan
@@ -157,7 +157,15 @@ abstract const class DraftMod : WebMod
     // lookup file
     file := pod.file(`/` + req.uri[2..-1].pathOnly, false)
     if (file == null) throw DraftErr(404)
-    FileWeblet(file).onService
+
+    // FileWeblet seems ripe for Broken pipe errors; so catch
+    // and ignore to avoid spamming error logs
+    try { FileWeblet(file).onService }
+    catch (IOErr err)
+    {
+      if (err.msg.contains("java.net.SocketException: Broken pipe")) return
+      throw err
+    }
   }
 
 //////////////////////////////////////////////////////////////////////////

commit 3bace1a133f146b75b8af231a84b7e31eeb4daf8

Move broken pipe check from c8e9928 to top level `onService`
Seeing this in other cases; so lets just trap for everyone

src/fan/DraftMod.fan
@@ -124,6 +124,10 @@ abstract const class DraftMod : WebMod
     }
     catch (Err err)
     {
+      // do not spam logs with broken pipe errs
+      if (err.msg.contains("java.net.SocketException: Broken pipe")) return
+
+      // wrap in DraftErr and log
       if (err isnot DraftErr) err = DraftErr(500, err)
       logErr(err)
       onErr(err)
@@ -157,15 +161,7 @@ abstract const class DraftMod : WebMod
     // lookup file
     file := pod.file(`/` + req.uri[2..-1].pathOnly, false)
     if (file == null) throw DraftErr(404)
-
-    // FileWeblet seems ripe for Broken pipe errors; so catch
-    // and ignore to avoid spamming error logs
-    try { FileWeblet(file).onService }
-    catch (IOErr err)
-    {
-      if (err.msg.contains("java.net.SocketException: Broken pipe")) return
-      throw err
-    }
+    FileWeblet(file).onService
   }
 
 //////////////////////////////////////////////////////////////////////////